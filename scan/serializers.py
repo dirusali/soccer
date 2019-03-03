dc36483f37dc638aa18279d3edd5738053b0e788
Diego <dirusali@gmail.com>
2019-03-03 12:57:35 +0100

    Create serializers.py

---
+++ scan/serializers.py
@@ -0,0 +1,12 @@
+rom rest_framework import serializers
+from scan.models import 
+
+class TeamSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Team
+        fields = '__all__'
+
+class MatchSerializer(serializers.HyperlinkedModelSerializer):    
+    class Meta:
+        model = Match
+        fields = '__all__'
