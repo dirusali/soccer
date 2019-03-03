b930baeb94e6b03d1c0582159c8d6f313e0bab57
Diego <dirusali@gmail.com>
2019-03-03 13:02:36 +0100

    Update serializers.py

--- scan/serializers.py
+++ scan/serializers.py
@@ -2,11 +2,14 @@ from rest_framework import serializers
 from scan.models import Team, Match
 
 class TeamSerializer(serializers.HyperlinkedModelSerializer):
+    team = TeamSerializer(required = False)
     class Meta:
         model = Team
         fields = '__all__'
 
-class MatchSerializer(serializers.HyperlinkedModelSerializer):    
+class MatchSerializer(serializers.HyperlinkedModelSerializer):  
+    match = MatchSerializer(required = False)
+
     class Meta:
         model = Match
         fields = '__all__'
