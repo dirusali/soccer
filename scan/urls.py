88f6f827d6048c9c18bf0d35b0b96c49ea0d047e
Diego <dirusali@gmail.com>
2019-03-03 13:22:01 +0100

    Update urls.py

--- scan/urls.py
+++ scan/urls.py
@@ -4,6 +4,7 @@ from . import views
 from django.conf.urls import url, include
 from django.contrib.auth import views as auth_views
 
+from django.contrib.auth.views.LoginView import LoginView
 
 
 router = routers.DefaultRouter()
