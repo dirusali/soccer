3347fea6c17d7dbf6bbfc8852dc2309ea427ae93
Diego <dirusali@gmail.com>
2019-03-03 19:09:23 +0100

    Update load_players.py

--- scan/management/commands/load_players.py
+++ scan/management/commands/load_players.py
@@ -14,16 +14,17 @@ class Command(BaseCommand):
         equipos = json.loads(r.text)['teams']
         teams = Team.objects.all()
         for i in teams:
-            i.identificador = 
-        for i in equipos:
-            id = str(i['id'])
+            id = str(i.identificador)
             url = 'http://api.football-data.org/v2/teams/' + id
             response = equests.get(url, headers={'X-Auth-Token':'dfec1fbedad7421abdad5eda2372b4c2'})
             squad = json.loads(r.text)['squad']
             for p in squad:
                 if p['role'] == 'PLAYER':
                     edad = 2019 - int(p['dateOfBirth'][0:4])
-                    Team.player_set.create(name=i['name'],position=i['position'],age=edad)
+                    i.player_set.create(
+                    name=i['name'],
+                    position=i['position'],
+                    age=edad)
                     print('Created player %s' % name)
-                    
+             print('CREATED PLAYERS FOR TEAM %s' % i.name)
     print('process finished')        
