706eeb8bffa37405bf0569ddacd306d114dc7e74
Diego <dirusali@gmail.com>
2019-03-03 18:05:55 +0100

    Create load_teams.py

---
+++ scan/management/commands/load_teams.py
@@ -0,0 +1,19 @@
+from django.core.management.base import BaseCommand, CommandError
+from scan.models import Competition, Match, Team, Player, Lineup
+
+import requests
+import csv
+
+url = 'http://api.football-data.org/v2/competitions/2014/teams'
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        r = requests.get(url, headers={'X-Auth-Token':'dfec1fbedad7421abdad5eda2372b4c2'})
+        equipos = json.loads(r.text)['teams']
+        for i in equipos:
+            nombre = i['shortName']
+            Team.create(name= nombre)
+            print('CREATED TEAM %s' % nombre)
+    print('process finished')        
+        
