9c7f51df515d6199e273639cf7f55761c9dbe8c4
Diego <dirusali@gmail.com>
2019-03-03 18:11:06 +0100

    Update load_teams.py

--- scan/management/commands/load_teams.py
+++ scan/management/commands/load_teams.py
@@ -1,4 +1,5 @@
 from django.core.management.base import BaseCommand, CommandError
+from django.db import models
 from scan.models import Competition, Match, Team, Player, Lineup
 
 import requests
@@ -11,9 +12,11 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         r = requests.get(url, headers={'X-Auth-Token':'dfec1fbedad7421abdad5eda2372b4c2'})
         equipos = json.loads(r.text)['teams']
+        count = 0
         for i in equipos:
+            count+=1
             nombre = i['shortName']
-            Team.create(name= nombre)
+            Team.objects.create(name= nombre,pk=count)
             print('CREATED TEAM %s' % nombre)
     print('process finished')        
         
