5945e31a1a5b903b5ba0403558ee157eb08f3a8e
Diego <dirusali@gmail.com>
2019-03-03 19:22:05 +0100

    Update load_players.py

--- scan/management/commands/load_players.py
+++ scan/management/commands/load_players.py
@@ -14,17 +14,21 @@ class Command(BaseCommand):
         equipos = json.loads(r.text)['teams']
         teams = Team.objects.all()
         for i in teams:
-            id = str(i.identificador)
-            url = 'http://api.football-data.org/v2/teams/' + id
-            response = requests.get(url, headers={'X-Auth-Token':'dfec1fbedad7421abdad5eda2372b4c2'})
-            squad = json.loads(response.text)['squad']
-            for p in squad:
-                if p['role'] == 'PLAYER':
-                    edad = 2019 - int(p['dateOfBirth'][0:4])
-                    i.player_set.create(name = p['name'], position = p['position'], age = edad)
-                    print('Created player %s' % p['name'])
-                if p['role'] == 'COACH':
-                    i.coach = p['name']
-                    i.save()
+            try:
+                id = str(i.identificador)
+                url = 'http://api.football-data.org/v2/teams/' + id
+                response = requests.get(url, headers={'X-Auth-Token':'dfec1fbedad7421abdad5eda2372b4c2'})
+                squad = json.loads(response.text)['squad']
+                for p in squad:
+                    if p['role'] == 'PLAYER':
+                        edad = 2019 - int(p['dateOfBirth'][0:4])
+                        i.player_set.create(name = p['name'], position = p['position'], age = edad)
+                        print('Created player %s' % p['name'])
+                    if p['role'] == 'COACH':
+                        i.coach = p['name']
+                        i.save()
+            except Exception as e:
+                print(e)
+                pass
             print('CREATED PLAYERS FOR TEAM %s' % id)
     print('process finished')        
