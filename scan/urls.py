4b0634f06b891ffd1e2b094efecbca85eb542dee
Diego <dirusali@gmail.com>
2019-03-03 11:56:24 +0100

    Create urls.py

---
+++ scan/urls.py
@@ -0,0 +1,14 @@
+
+from rest_framework import routers
+from . import views
+
+router = routers.DefaultRouter()
+router.register(r'competitions', views.ProViewSet)
+router.register(r'matches', views.ShopViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
+urlpatterns = [
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+]
