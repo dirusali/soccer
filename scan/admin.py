2503cc7524ec86459aa7442c5124520c57e898f6
Diego <dirusali@gmail.com>
2019-03-03 16:51:25 +0100

    Update admin.py

--- scan/admin.py
+++ scan/admin.py
@@ -3,14 +3,14 @@ from django.contrib import admin
 
 
 class CompetitionAdmin(admin.ModelAdmin):
-    list_display = ('name')
+    list_display = ['name']
     search_fields = ('name',)
 
 admin.site.register(Competition, CompetitionAdmin)
 
 
 class MatchAdmin(admin.ModelAdmin):
-    list_display = ('teams', 'matchday')
+    list_display = ('matchday')
     list_filter = ('matchday',)
     
 admin.site.register(Match, MatchAdmin)
@@ -23,7 +23,7 @@ class TeamAdmin(admin.ModelAdmin):
 admin.site.register(Team, TeamAdmin)
 
 class PlayerAdmin(admin.ModelAdmin):
-    list_display = ('name',)
+    list_display = ('name')
     list_filter = ('name',)
 
 admin.site.register(Player, PlayerAdmin)
