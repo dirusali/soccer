dff1ac8b1543beac216976c9696031acfe5267e2
Diego <dirusali@gmail.com>
2019-03-12 22:57:09 +0100

    Update admin.py

--- scan/admin.py
+++ scan/admin.py
@@ -29,6 +29,6 @@ class PlayerAdmin(admin.ModelAdmin):
 admin.site.register(Player, PlayerAdmin)
 
 class LineupAdmin(admin.ModelAdmin):
-    list_display = ['name', 'lineupid', 'team', 'club', 'timeplayed', 'goalsfavor', 'goalscounter']
+    list_display = ['name', 'club', 'timeplayed', 'goalsfavor', 'goalscounter']
     
 admin.site.register(Lineup, LineupAdmin)
