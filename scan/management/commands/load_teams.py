ba178961f0dfea7aaa257f1598462ef9548e947e
Diego <dirusali@gmail.com>
2019-03-19 18:02:03 +0100

    Update load_teams.py

--- scan/management/commands/load_teams.py
+++ scan/management/commands/load_teams.py
@@ -5,7 +5,7 @@ from scan.models import Competition, Match, Team, Player, Lineup
 import requests
 import json
 
-url = 'http://api.football-data.org/v2/competitions/2014/teams?season=2017'
+url = 'http://api.football-data.org/v2/competitions/2014/teams?season=2016'
 
 
 class Command(BaseCommand):
