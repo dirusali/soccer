0c764671f6e4e2066e939bdd3a0ec3cb0b30a0f2
Diego <dirusali@gmail.com>
2019-03-23 15:18:29 +0100

    Update urls.py

--- portal/urls.py
+++ portal/urls.py
@@ -6,7 +6,7 @@ from .views import home
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', home, name='home'),
+    url(r'^partidos/', partidos, name='partidos'),
     
 ]    
     
