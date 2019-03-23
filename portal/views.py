bb44bc896b57e5830c6fd18b561a14f6b84ee6b9
Diego <dirusali@gmail.com>
2019-03-23 15:23:25 +0100

    Update views.py

--- portal/views.py
+++ portal/views.py
@@ -6,4 +6,4 @@ from scan.models import Match
 def partidos(request):
     qs = Match.objects.all()        
     vista = {"partidos": "qs"}
-    return HttpResponse(request, 'partidos.html', context=vista)
+    return HttpResponse(request, 'partidos.html', vista)
