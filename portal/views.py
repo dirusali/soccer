08a93a7048764a7a7e0f327186cb9d160e760544
Diego <dirusali@gmail.com>
2019-03-23 10:37:36 +0100

    Update views.py

--- portal/views.py
+++ portal/views.py
@@ -1,19 +1,4 @@
-import functools
-import operator
 
-from django.shortcuts import render
-from django.views.generic.detail import DetailView
-from django.views import View
-from django.db.models import Count
-
-from scan.models import Lineup, Player,Team
-from scan.serializers import SearchTagSearializer
-
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
-
-from datetime import datetime
 
 from django.http import HttpResponse
 
