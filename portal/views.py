5978c67c0eb52f6a5b75339c62f0d494b8743a77
Diego <dirusali@gmail.com>
2019-03-23 15:53:11 +0100

    Update views.py

--- portal/views.py
+++ portal/views.py
@@ -5,12 +5,12 @@ from scan.models import Match, Team
 
 def partidos(request):
     actual = 246112 + 280
-    locals = []
+    locales = []
     visitors = []
     qs = Match.objects.all()
     for i in range(actual, actual+10):
         p = Match.objects.get(matchid=i)
-        locals.append(p.local.name)
+        locales.append(p.local.name)
         visitors.append(p.visitor.name)
     vista = {'matches':qs}
     return render(request, 'partidos.html', context=vista)
