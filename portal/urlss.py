27bf8e081481e38a58030e9dc5971343d8ad1598
Diego <dirusali@gmail.com>
2019-03-23 10:28:09 +0100

    Rename urls.py to urlss.py

---
+++ portal/urlss.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from django.contrib import admin
+
+
+from .views import home
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^$', home, name='home'),
+    
