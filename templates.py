648dd60ce88117976b29c49b24189350d1b1adc4
Diego <dirusali@gmail.com>
2019-03-23 15:09:09 +0100

    Update templates.py

--- templates.py
+++ templates.py
@@ -1,2 +1,12 @@
 <h1>PARTIDOS DE LA JORNADA 29</h1>"
 
+<p> PARTIDO 1 <P>  {{local1}} -  {{visitante1}}
+<p> PARTIDO 2 <P>  {{local1}} -  {{visitante1}
+<p> PARTIDO 3 <P>  {{local1}} -  {{visitante1} 
+<p> PARTIDO 4 <P>  {{local1}} -  {{visitante1}
+<p> PARTIDO 5 <P>  {{local1}} -  {{visitante1} 
+<p> PARTIDO 6 <P>  {{local1}} -  {{visitante1}
+<p> PARTIDO 7 <P>  {{local1}} -  {{visitante1}
+<p> PARTIDO 8 <P>  {{local1}} -  {{visitante1}
+<p> PARTIDO9 <P>   {{local1}} -  {{visitante1}
+<p> PARTIDO 10 <P> {{local1}} -  {{visitante1}
