f956b7ff337a5a61d528c1139bef1071d4a2be81
Diego <dirusali@gmail.com>
2019-03-23 10:57:11 +0100

    Update urls.py

--- scan/urls.py
+++ scan/urls.py
@@ -1,20 +1,20 @@
-
 from rest_framework import routers
 from . import views
 from django.conf.urls import url, include
 from django.contrib.auth import views as auth_views
 
+from .views import postmodelview
 
 
 router = routers.DefaultRouter()
 router.register(r'teams', views.TeamViewSet)
 router.register(r'matches', views.MatchViewSet)
 
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
+
 urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url( r'^login/$',auth_views.LoginView.as_view(template_name="useraccounts/login.html"), name="login")
+    url(r'^login/$',auth_views.LoginView.as_view(template_name="useraccounts/login.html"), name="login")
+    url(r'^apuestas/ apuestas, name=apuestas)
 
 ]
