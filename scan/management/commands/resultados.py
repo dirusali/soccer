c406220b3ef9c665b748949d8e37e46b65e7bfed
Diego <dirusali@gmail.com>
2019-06-09 17:22:05 +0200

    Update resultados.py

--- scan/management/commands/resultados.py
+++ scan/management/commands/resultados.py
@@ -5,8 +5,25 @@ from scan.models import Competition, Match, Team, Player, Lineup
 import requests
 import json
 
-url = 'http://api.football-data.org/v2/competitions/2014/teams?season=2016'
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
+        local_lineup_name = input("Indica la alineacion local: ")
+        visitor_lineup_name = input("Indica la alineacion visitante: ")
+        local = Lineups.objects.get(name=local_lineup_name)
+        visitor = Lineups.objects.get(name=visitor_lineup_name)
+        localplayers=Lineups.objects.get(name=local_lineup_name).values('players')
+        visitorplayers = Lineups.objects.get(name=visitor_lineup_name).values('players')
+        print('El equipo local juega con:')
+        for i in localplayers:
+            p =i.get('players',None)
+            player = Players.objects.get(id=p.id)
+            print(player.name)   
+        print('El equipo visitante juega con:')
+            for i in visitorplayers:
+            p = i.get('players',None)
+            player = Players.objects.get(id=p.id)
+            print(player.name)
+        
+
