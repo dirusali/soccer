f8b0c8a3281d1219d22184e34279c35b970b7508
Diego <dirusali@gmail.com>
2019-06-09 17:28:25 +0200

    Update resultados.py

--- scan/management/commands/resultados.py
+++ scan/management/commands/resultados.py
@@ -11,19 +11,19 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         local_lineup_name = input("Indica la alineacion local: ")
         visitor_lineup_name = input("Indica la alineacion visitante: ")
-        local = Lineups.objects.get(name=local_lineup_name)
-        visitor = Lineups.objects.get(name=visitor_lineup_name)
-        localplayers=Lineups.objects.get(name=local_lineup_name).values('players')
-        visitorplayers = Lineups.objects.get(name=visitor_lineup_name).values('players')
+        local = Lineup.objects.get(name=local_lineup_name)
+        visitor = Lineup.objects.get(name=visitor_lineup_name)
+        localplayers=Lineup.objects.get(name=local_lineup_name).values('players')
+        visitorplayers = Lineup.objects.get(name=visitor_lineup_name).values('players')
         print('El equipo local juega con:')
         for i in localplayers:
             p =i.get('players',None)
-            player = Players.objects.get(id=p.id)
+            player = Player.objects.get(id=p.id)
             print(player.name)   
         print('El equipo visitante juega con:')
         for i in visitorplayers:
             p = i.get('players',None)
-            player = Players.objects.get(id=p.id)
+            player = Player.objects.get(id=p.id)
             print(player.name)
         
 
